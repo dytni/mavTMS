7fa4305b9c3d97a9f827e5fcb5f763ed67b5711d
dytni <nikolajolesko391@gmail.com>
2023-11-22 02:50:49 +0300

    hi3

---
+++ src/main/java/by/dytni/webapp/MainServlet.java
@@ -0,0 +1,45 @@
+package by.dytni.webapp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "MainServlet",
+        urlPatterns = "/main"
+)
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.setContentType("text/html; charset=UTF-8");
+        final String body = """
+                <!DOCTYPE html>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+                <html lang="en">
+                <body>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand">My Homeworks</a>
+                    </div>
+                </nav>
+                <div class="dropdown" >
+                    <button class="btn btn-secondary dropdown-toggle" style="width: 100%; height: 10%;" type="button" data-bs-toggle="dropdown" aria-expanded="false">My tasks</button>
+                    <ul class="dropdown-menu" style="width: 100%;">
+                        <li><button type="button"  onclick="location.href='time'" class="dropdown-item">time</button></li>
+                        <li><button type="button" onclick="location.href='names'" class="dropdown-item">name</button></li>
+                    </ul>
+                </div>
+                </body>
+                </html>
+                """;
+        resp.getWriter().println(body);
+    }
+}
+
--- src/main/java/by/dytni/webapp/NameServlet.java
+++ src/main/java/by/dytni/webapp/NameServlet.java
@@ -31,7 +31,36 @@ public class NameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write(names.toString());
+        resp.setContentType("text/html; charset=UTF-8");
+        String body = """
+                <!DOCTYPE html>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+                <html lang="en">
+                <body>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand">Names</a>
+                    </div>
+                </nav>
+                <ul class="list-group">
+                """;
+        StringBuffer stringBuffer = new StringBuffer(body);
+        for (String name:names) {
+            stringBuffer.append("<li class=\"list-group-item\">")
+                    .append(name)
+                    .append("<form action=\"names\\")
+                    .append(name)
+                    .append("""
+                            ">
+                             <button type="button" formmethod="post" class="badge bg-black pill">delete</button></form></li>
+                            """);
+        }
+        stringBuffer.append("""
+                </ul>
+                </body>
+                </html>""");
+        resp.getWriter().println(stringBuffer);
     }
 
     @Override
